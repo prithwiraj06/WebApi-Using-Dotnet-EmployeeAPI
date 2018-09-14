b2519945b12965f5339688ac883872996d0dfdf9
prithwiraj06 <prethwiraj06@gmail.com>
2018-09-14 22:10:51 +0530

    WebApi-EmployeeApi

---
+++ AngularWebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AngularWebAPI.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEnumerable<tblEmployee> Get()
+        {
+            using (AngularWebAPIEntities dbcontext = new AngularWebAPIEntities())
+            {
+                return dbcontext.tblEmployees.ToList();
+            }
+        }
+        public tblEmployee Get(string id)
+        {
+            using (AngularWebAPIEntities dbcontext = new AngularWebAPIEntities())
+            {
+                return dbcontext.tblEmployees.FirstOrDefault(x=>x.Id==id);
+            }
+        }
+
+    }
+}
